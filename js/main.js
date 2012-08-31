9a3d879c8471d26de8c327a48a4f8bf4d6c68208
Nick Williams <WickyNilliams@MBA>
2012-08-31 19:25:39 +0100

    declarative scroll spy, removed needless robots.txt, added google site verification

--- js/main.js
+++ js/main.js
@@ -7,35 +7,28 @@ $(function() {
 	var $toc = $("<div class='toc nav sidebar' data-spy='affix' data-offset-top='110' data-offsetop-bottom='200'><h2 class='title'>Jump To&hellip;</h2></div>"),
 		$main = $("[role=main], .main");
 
-	function setup() {
-		Modernizr.load([
-			{
-				load: "js/jquery.toc.js",
-				callback : function() {
-					$main.toc({
-						container : "<ul class='no-bullets'></ul>",
-						destination: $toc,
-						targets : ["h2"]
-					});
-					$("body").append($toc);
-				}
-			},
-			{
-				load : "js/vendor/bootstrap-scrollspy.js",
-				callback : function() {
-					$("body").scrollspy({offset: 50});
-				}
-			},
-			"js/vendor/bootstrap-affix.js"
-		]);
-	}
-
 
 	enquire.register("screen and (min-width:1310px)", [{
 
 		deferSetup : true,
 
-		setup : setup,
+		setup : function setup() {
+			Modernizr.load([
+				{
+					load: "js/jquery.toc.js",
+					callback : function() {
+						$main.toc({
+							container : "<ul class='no-bullets'></ul>",
+							destination: $toc,
+							targets : ["h2"]
+						});
+						$("body").append($toc);
+					}
+				},
+				"js/vendor/bootstrap-scrollspy.js",
+				"js/vendor/bootstrap-affix.js"
+			]);
+		},
 
 		match : function() {
 			$toc.fadeIn();
