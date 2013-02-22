117baa52dd5a452f03b8827f3a414e2eb6c998e3
WickyNilliams <WickyNilliams@MBA>
2013-02-22 02:11:09 +0000

    further refinements of styling and markup

--- js/main.js
+++ js/main.js
@@ -4,39 +4,35 @@
 
 $(function() {
 
-	var $toc = $("<nav class='toc nav sidebar' data-spy='affix' data-offset-top='110' data-offsetop-bottom='200' role='navigation'><h2 class='section-title'>Jump To&hellip;</h2></nav>"),
-		$main = $("[role=main], .main");
+	var $toc = $("<nav class='nav sidebar' data-spy='affix' data-offset-top='75' data-offsetop-bottom='200' role='navigation'><h2 class='toc__title section-title'>Jump To&hellip;</h2></nav>"),
+		$main = $("[role=main], .main"),
+		load;
 
-
-	enquire.register("screen and (min-width:1100px)", [{
-
-		deferSetup : true,
-
-		setup : function setup() {
-			Modernizr.load([
-				{
-					load: "js/jquery.toc.js",
-					callback : function() {
-						$main.toc({
-							container : "<ul class='no-bullets'></ul>",
-							destination: $toc,
-							targets : ["h2"]
-						});
-						$("body").append($toc);
-					}
-				},
-				"js/vendor/bootstrap-scrollspy.js",
-				"js/vendor/bootstrap-affix.js"
-			]);
+	load = [{
+		load: "js/jquery.toc.js",
+		callback : function() {
+			$main.toc({
+				container : "<ul class='toc no-bullets'></ul>",
+				item : "<li class='toc__item'><a></a></li>",
+				destination: $toc,
+				targets : ["h2"]
+			});
+			$("body").prepend($toc);
+		}
 		},
+		"js/vendor/bootstrap-scrollspy.js",
+		"js/vendor/bootstrap-affix.js"
+	];
 
+
+	enquire.register("screen and (min-width:70em)", {
+		deferSetup : true,
+		setup : load,
 		match : function() {
 			$toc.fadeIn();
 		},
-
 		unmatch : function() {
 			$toc.hide();
 		}
-	}]).listen(10);
-
+	});
 });
