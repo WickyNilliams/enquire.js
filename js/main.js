95a45b805f8959794093d45d607a80c03c199b59
WickyNilliams <WickyNilliams@MBA>
2013-03-11 18:33:45 +0000

    first pass of proper API documentation

--- js/main.js
+++ js/main.js
@@ -4,27 +4,24 @@
 
 (function() {
 
-	var load, $toc, $main;
+	var $toc, $main, load;
 
 	load = [
 		{
 			load : "js/vendor/zepto.js",
-			callback: function() { window.jQuery = window.$ = window.Zepto;}},
+			callback: function() { window.jQuery = window.$ = window.Zepto;}
+		},
 		{
 			load: "js/jquery.toc.js",
 			callback : function() {
 				$toc  = $("<nav class='nav sidebar' data-spy='affix' data-offset-top='75' role='navigation'><h2 class='toc__title section-title'>Jump To&hellip;</h2></nav>"),
-				$main = $("[role=main], .main");
-				$main.toc({
-					destination: $toc,
-					targets : ["h2"]
-				});
+					$main = $("[role=main], .main");
+				$main.toc({ destination: $toc, targets : ["h2"] });
 				$("body").prepend($toc);
 			}
 		},
 		"js/vendor/bootstrap.js"
 	];
-
 
 	enquire.register("screen and (min-width:75em)", {
 		deferSetup : true,
