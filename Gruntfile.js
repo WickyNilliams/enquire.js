bf5532d23c3c4a1b31529d146cfcc9ec8f86668b
WickyNilliams <WickyNilliams@MBA>
2013-04-14 23:17:07 +0100

    add gzip reporting to uglify build step

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,7 +38,8 @@ module.exports = function(grunt) {
 
         uglify: {
             options : {
-                banner : '<%= meta.banner %>'
+                banner : '<%= meta.banner %>',
+                report: 'gzip'
             },
             dist: {
                 files : {
