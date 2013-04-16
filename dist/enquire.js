f6ee234fb62167dcd72de40f8d033ad2708cf135
WickyNilliams <WickyNilliams@MBA>
2013-04-16 21:35:39 +0100

    drop alpha from version number

--- dist/enquire.js
+++ dist/enquire.js
@@ -1,4 +1,4 @@
-// enquire.js v2.0.0a - Awesome Media Queries in JavaScript
+// enquire.js v2.0.0 - Awesome Media Queries in JavaScript
 // Copyright (c) 2013 Nick Williams - http://wicky.nillia.ms/enquire.js
 // License: MIT (http://www.opensource.org/licenses/mit-license.php)
 
