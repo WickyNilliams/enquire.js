e6f139ee9aa467cb9fc04bb08fb1f51a4a14068a
WickyNilliams <WickyNilliams@MBA>
2013-07-22 20:19:35 +0100

    fix banner and bump version numbers

--- dist/enquire.js
+++ dist/enquire.js
@@ -1,6 +1,8 @@
-// enquire.js v2.0.2 - Awesome Media Queries in JavaScript
-// Copyright (c) 2013 Nick Williams - http://wicky.nillia.ms/enquire.js
-// License: MIT (http://www.opensource.org/licenses/mit-license.php)
+/*!
+ * enquire.js v2.0.3 - Awesome Media Queries in JavaScript
+ * Copyright (c) 2013 Nick Williams - http://wicky.nillia.ms/enquire.js
+ * License: MIT (http://www.opensource.org/licenses/mit-license.php)
+ */
 
 ;(function (name, context, factory) {
 	if (typeof module !== 'undefined' && module.exports) {
