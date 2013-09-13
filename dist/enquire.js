e357232c9fcc633bf6016109caedb7cb35780637
WickyNilliams <WickyNilliams@MBA>
2013-09-13 18:51:31 +0100

    bump version for umd support

--- dist/enquire.js
+++ dist/enquire.js
@@ -1,5 +1,5 @@
 /*!
- * enquire.js v2.0.3 - Awesome Media Queries in JavaScript
+ * enquire.js v2.1.0 - Awesome Media Queries in JavaScript
  * Copyright (c) 2013 Nick Williams - http://wicky.nillia.ms/enquire.js
  * License: MIT (http://www.opensource.org/licenses/mit-license.php)
  */
