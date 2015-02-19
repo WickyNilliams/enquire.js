7576e07710040eaf68ae21634ed4acb1f1bcb9b2
Patrick Scott <patrick.lee.scott@gmail.com>
2015-02-19 01:26:00 -0500

    better way to add meteor support

---
+++ meteor/package.js
@@ -0,0 +1,29 @@
+// package metadata file for Meteor.js
+'use strict';
+
+var packageName = 'patrickleet:enquire';  // https://atmospherejs.com/patrickleet/enquire
+
+var packageJson = JSON.parse(Npm.require("fs").readFileSync('package.json'));
+
+Package.describe({
+  name: packageName,
+  summary: "A lightweight library for programmatically responding to media queries.",
+  version: packageJson.version,
+  git: 'https://github.com/enquire/enquire.git'
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom(['METEOR@0.9.0', 'METEOR@1.0']);
+  api.export('enquire');
+  api.addFiles([
+    'dist/enquire.js',
+    'meteor/export.js'
+  ]);
+});
+
+Package.onTest(function (api) {
+  api.use(packageName);
+  api.use('tinytest');
+
+  api.addFiles('meteor/test.js');
+});
---
+++ meteor/test.js
@@ -0,0 +1,5 @@
+'use strict';
+// Add Tests
+Tinytest.add('Enquire.loaded', function (test) {
+  test.ok(typeof enquire === 'function');
+});
