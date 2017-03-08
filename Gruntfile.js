64630dc5afa7e86da5e8477c7ac6a340b4e8dc80
WickyNilliams <WickyNilliams@MBA>
2017-03-08 17:03:17 +0000

    let jshint find config files

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
 
         jshint: {
             options: {
-                jshintrc : '.jshintrc'
+                jshintrc : true,
             },
             prebuild : [
                 'Gruntfile.js',
